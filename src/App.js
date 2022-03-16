5a02b97ab636ecf8e8f5ba73a658ea0ebc361c18
Synui <jonathon.schoenholz@yahoo.com>
2022-03-16 13:23:09 -0400

    About and Nav index.js updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// center of the application
+// the root component, or the wrapper component that houses all of the other components
+import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // React.createElement("div", {}, [])
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function Nav() {
+    const categories = [
+        {
+            name: "Commercial",
+            description:
+                "Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "Portraits", description: "Portraits of people in my life" },
+        { name: "Food", description: "Delicious delicacies" },
+        {
+            name: "Landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ];
+    /* console log the clicked names in nav bar */
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {/* Whenever we map over anything in JSX, the outermost element must have a key attribute that's set to be something unique */}
+                    {categories.map((category) => (
+                        <li
+                            className="mx-1"
+                            key={category.name}
+                        >
+                            {/* on click utilize categorySelected function */}
+                            <span onClick={() => categorySelected(category.name)} >
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;

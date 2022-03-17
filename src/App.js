dc19965d1dbbeb413acb98e3ce06ff65d707dcdc
Synui <jonathon.schoenholz@yahoo.com>
2022-03-17 18:02:35 -0400

    Navbar conditional update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // center of the application
 // the root component, or the wrapper component that houses all of the other components
 import React, { useState } from 'react';
-import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
@@ -14,18 +13,35 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  // We set the initial value of contactSelected to false to prevent the contact form from showing when a user initially navigates to the homepage
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {/* This conditional declare that if the contactSelected is false, the Gallery and About components should be rendered, 
+        but if contactedSelected is true, the ContactForm component should be rendered. */}
+        {!contactSelected ? (
+          // React fragments: a tool in React to allow multiple elements to be grouped together
+          // Although in JSX you can only return a single parent element, this rule can be satisfied by wrapping the children components in a React fragment
+          // This also allows you to wrap elements without creating extra DOM nodes
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+          // : is a ternary operator that enables conditional rendering for React
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({
@@ -7,12 +8,32 @@ function ContactForm() {
         message: ''
     });
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const { name, email, message } = formState;
 
     function handleChange(e) {
+        // This conditional statement says if the <input> is email, then validate the value of that input field with the validateEmail function and assign its return to isValid
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
         // The name property of target actually refers to the name attribute of the form element. 
         // This attribute value matches the property names of formState (name, email, and message) and allows us to use [ ] to create dynamic property names.
-        setFormState({ ...formState, [e.target.name]: e.target.value })
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+        // console.log('errorMessage', errorMessage);
     };
     // console.log(formState) is located outside the handleChange function declaration
     // because the asynchronous nature of the setFormState function will cause the console.log placed in the function body of handleChange to appear delayed in its ability to sync
@@ -31,18 +52,28 @@ function ContactForm() {
                 {/* name input */}
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 {/* email input */}
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+                    <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
                 </div>
                 {/* message text area */}
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
                 </div>
+                {/* Same as a conditional "if" statement.
+                If errorMessage has a truthy value, the <div> block will render. 
+                If errorMessage doesn't have an error message, the following <div> block doesn't render. 
+                The && operator—known as the AND operator—is being used here as a short circuit. 
+                If the first value resolves to true, the second expression is evaluated.*/}
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>

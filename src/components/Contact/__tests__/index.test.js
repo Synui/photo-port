ae881ece0fb4ea8fdaccb9dedefc12c1d89ec24e
Synui <jonathon.schoenholz@yahoo.com>
2022-03-17 18:22:36 -0400

    Test for Contact complete

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable jest/no-identical-title */
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+
+afterEach(cleanup);
+
+
+describe('Contact component renders', () => {
+    it('renders', () => {
+        render(<Contact />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Contact />)
+        expect(asFragment()).toMatchSnapshot()
+    });
+});
+
+it('renders', () => {
+    const { getByTestId } = render(<Contact />)
+    expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+  })
+   
+  it('renders', () => {
+    const { getByTestId } = render(<Contact />)
+    expect(getByTestId('button')).toHaveTextContent('Submit')
+  })
